99bd75f32a30953d034e1742f7c0243dfba64710
Josephine Valensia <jval0011@student.monash.edu>
2022-08-21 15:48:24 +1000

    combine 2 mode in app.r

--- app.R
+++ app.R
@@ -1,132 +1,186 @@
-library(shiny)
 library(tidyverse)
+library(shiny)
+library(plotly)
+library(DT)
 
+
+# Define UI for application that draws a histogram
 ui <- fluidPage(
 
-    titlePanel("Old Faithful Geyser Data"),
+  tags$head(
+    tags$style(HTML('.shiny-split-layout>div {overflow: hidden;}')),
+  ),
 
-    h3("1. What number of bins do you stop seeing bimodality in the waiting time?"),
-    fluidRow(
-      sidebarLayout(
-          sidebarPanel(
-              sliderInput("bins",
-                          "Number of bins:",
-                          min = 1,
-                          max = 50,
-                          value = 30)
+  # Application title
+  titlePanel("Is Your Favorite Airline Safe?"),
+
+  tabsetPanel(
+    tabPanel("Compare Mode",
+
+  # Sidebar with options to compare type of events of Airline1 and Airline 2
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("aero1",
+                  "Choose a Airline 1:",
+                  unique(air$airline),
+                  selected = "Aer Lingus"),
+      selectInput("aero2",
+                  "Choose a Airline 2:",
+                  unique(air$airline),
+                  selected = "Aeroflot"),
+      radioButtons("event1",
+                   "Types of Event:",
+                   c("Incidents" = "incidents",
+                     "Fatal Accidents" = "fatal_accidents",
+                     "Fatalities" = "fatalities"),
+                   selected = "incidents")
+    ),
+
+    # showing info of airline 1 and airline 2
+    mainPanel(
+      splitLayout(
+        verticalLayout(
+          fluidRow(
+            column(8,
+                   br(),
+                   h2(textOutput("aeroname1"))),
+            column(4, uiOutput("aeropic1"))
           ),
-
-          mainPanel(
-             plotOutput("distPlot")
-          )
+          plotlyOutput("incPlot1"),
+          h3("Rating"),
+          textOutput("aerorate1")),
+        verticalLayout(
+          fluidRow(
+            column(8,
+                   br(),
+                   h2(textOutput("aeroname2"))),
+            column(4, uiOutput("aeropic2"))
+          ),
+          plotlyOutput("incPlot2"),
+          h3("Rating"),
+          textOutput("aerorate2"))
       )
-    ),
 
-    h3("2. How do the different geoms change the view of the data?"),
-    fluidRow(
-      sidebarLayout(
-        sidebarPanel(
-          radioButtons("geom",
-                      "Geom choice:",
-                      choices = c("geom_point",
-                                  "geom_density_2d",
-                                  "geom_density_2d_filled",
-                                  "geom_bin_2d",
-                                  "geom_hex"))
-        ),
+    )
+  )),
+  tabPanel("Rate Mode",
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput("rate",
+                    "Rating:",
+                    min = 1,
+                    max = 5,
+                    value = 3)
+      ),
 
-        mainPanel(
-          plotOutput("plot")
-        )
-      )
-    ),
-
-    h3("3. Is a mixture of two normal distribution good fit on eruption time?"),
-    fluidRow(
-      sidebarLayout(
-        sidebarPanel(
-          sliderInput("bins2",
-                      "Adjust the number of bins (if needed):",
-                      min = 1,
-                      max = 50,
-                      value = 30),
-          "Enter your guess for the:",
-          numericInput("p", "Mixing probability:",
-                       value = 0.35, min = 0, max = 1),
-          numericInput("mean1", "Mean of the first group:",
-                       value = 2.02),
-          numericInput("mean2", "Mean of the second group:",
-                       value = 4.27),
-          numericInput("sd1", "Standard deviation of the first group:",
-                       value = 0.24, min = 0),
-          numericInput("sd2", "Standard deviation of the second group:",
-                       value = 0.44, min = 0)
-        ),
-
-        mainPanel(
-          plotOutput("mixDistFit")
-        )
-      )
-    ),
+      # Show a plot of the generated distribution
+      mainPanel(
+        DT::dataTableOutput("rat")
+      ))
 
 
-    fluidRow(
-      column(10,
-             div(class = "about",
-                 uiOutput('about'))
-      )
-    ),
-    includeCSS("styles.css")
+
+  )
+
+
+
+  ),
+  includeCSS("styles.css")
 )
 
-server <- function(input, output) {
 
-  output$incPlot <- renderPlot({
-    air %>%
-      filter(airline == input$aero, type_of_event == "incidents") %>%
-      ggplot(aes(x = n_events,
-                 y = year_range)) +
-      geom_col(aes(fill = year_range)) +
-      labs(x = "Number of Events",
-           y = "Range of Year") +
-      scale_y_discrete(labels = c("85_99" = "1985 to 1999", "00_14" = "2000 to 2014")) +
-      theme_bw() +
-      theme(legend.position="none")
+server <- function(input, output, session) {
 
+  observe({
+    updateSelectInput(session, "aero2", choices = setdiff(unique(air$airline), input$aero1))
   })
 
+  output$aeroname1 <- reactive({input$aero1})
+  output$aeroname2 <- reactive({input$aero2})
 
-  output$distPlot <- renderPlot({
-        ggplot(faithful, aes(waiting)) +
-         geom_histogram(bins = input$bins, color = "white") +
-         theme_bw(base_size = 14) +
-         labs(x = "Waiting time", y = "Count")
-    })
+  output$aeropic1 <- renderUI({
+    pic1 <- air_pic %>%
+      filter(airline == input$aero1)
+    tags$img(src = pic1$logo, height="70%", width="70%")
+  })
 
-    output$plot <- renderPlot({
-      ggplot(faithful, aes(waiting, eruptions)) +
-        get(input$geom)() +
-        theme_bw(base_size = 14) +
-        labs(x = "Waiting time", y = "Eruption time")
-    })
+  output$aeropic2 <- renderUI({
+    pic2 <- air_pic %>%
+      filter(airline == input$aero2)
+    tags$img(src = pic2$logo, height="70%", width="70%")
+  })
 
-    output$mixDistFit <- renderPlot({
-      df <- data.frame(x = seq(min(faithful$eruptions), max(faithful$eruptions), length = 1000)) %>%
-        mutate(density = input$p * dnorm(x, input$mean1, input$sd1) +
-                         (1 - input$p) * dnorm(x, input$mean2, input$sd2))
+  y_label <- reactive({
+    req(input$event1)
+    if(input$event1 == "incidents"){
+      y_label <- "Number of Incidents"
+    } else if(input$event1 == "fatal_accidents"){
+      y_label <- "Number of Fatal Accidents"
+    } else if(input$event1 == "fatalities"){
+      y_label <- "Number of Fatalities"
+    }})
 
-      ggplot(faithful, aes(eruptions)) +
-        geom_histogram(aes(y = stat(density)), bins = input$bins2, color = "white") +
-        geom_line(data = df, aes(x = x, y = density), color = "red", size = 2) +
-        theme_bw(base_size = 14) +
-        labs(x = "Eruption time", y = "Density")
-    })
 
-    output$about <- renderUI({
-      knitr::knit("about.Rmd", quiet = TRUE) %>%
-        markdown::markdownToHTML(fragment.only = TRUE) %>%
-        HTML()
-    })
+  output$incPlot1 <- renderPlotly({
+    p1 <- air %>%
+      filter(airline == input$aero1, type_of_event == input$event1) %>%
+      ggplot(aes(y = n_events,
+                 x = year_range)) +
+      geom_col(aes(fill = year_range)) +
+      labs(y = y_label(),
+           x = "Range of Year") +
+      scale_x_discrete(labels = c("85_99" = "1985 to 1999", "00_14" = "2000 to 2014")) +
+      theme_bw() +
+      theme(legend.position="none")
+    ggplotly(p1)
+  })
+
+  output$incPlot2 <- renderPlotly({
+    p2 <- air %>%
+      filter(airline == input$aero2, type_of_event == input$event1) %>%
+      ggplot(aes(y = n_events,
+                 x = year_range)) +
+      geom_col(aes(fill = year_range)) +
+      labs(y = y_label(),
+           x = "Range of Year") +
+      scale_x_discrete(labels = c("85_99" = "1985 to 1999", "00_14" = "2000 to 2014")) +
+      theme_bw() +
+      theme(legend.position="none")
+    ggplotly(p2)
+  })
+
+  output$aerorate1 <- reactive({
+    stars1 <- air_stars %>%
+      filter(airline == input$aero1)
+    stars1$stars
+  })
+
+  output$aerorate2 <- reactive({
+    stars2 <- air_stars %>%
+      filter(airline == input$aero2)
+    stars2$stars
+  })
+
+  rateTable <- reactive({
+    req(input$rate)
+    if(input$rate == 1){
+      rateTable <- rat1
+    } else if(input$rate == 2){
+      rateTable <- rat2
+    } else if(input$rate == 3){
+      rateTable <- rat3
+    } else if(input$rate == 4){
+      rateTable <- rat4
+    } else if(input$rate == 5){
+      rateTable <- rat5
+    }
+  })
+
+  output$rat <- DT::renderDataTable({
+    DT::datatable(rateTable(), escape = FALSE)
+  })
+
 }
 
+# Run the application
 shinyApp(ui = ui, server = server)
